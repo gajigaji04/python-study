8dafedcd880f92e4c6b75de54cc6865f31c6d042
gajigaji04 <gajigaji04@naver.com>
2024-02-24 18:31:02 +0900

    1일차

---
+++ sectione-2/practice-numeric.py
@@ -0,0 +1,7 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3) # 8
+print(2*10) # 20
+print(3*(2*5)) # 30
---
+++ sectione-3/practice-간단한 수식.py
@@ -0,0 +1,22 @@
+print(2 + 5 * 4) # 22
+print((2 + 5) * 9) # 63
+number = 2 + 3 * 4 # 14
+print(number)
+
+number = number + 2
+print(number) # 16
+
+number += 2
+print(number) # 18
+
+number *= 2
+print(number) # 36
+
+number /= 2
+print(number) # 18
+
+number -= 2
+print(number) # 16
+
+number %= 5
+print(number) # 1
